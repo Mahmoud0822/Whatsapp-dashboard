6b8980f562971e2dd7dc3832a8d8910d59f90079
Bytee- <114771836+Vitin11a@users.noreply.github.com>
2025-03-14 21:57:33 -0300

    Adiciona configuração do Next.js

---
+++ frontend/next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['localhost', 'api.whatsapp.com', 'web.whatsapp.com'],
+  },
+  experimental: {
+    serverActions: true,
+  },
+};
+
+module.exports = nextConfig;
